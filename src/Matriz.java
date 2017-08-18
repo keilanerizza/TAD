476ecb9aa0301f04e0f5c85dde54cd34024c74b9
Keilane Rizza <keilanerizza@gmail.com>
2017-08-18 19:15:05 -0300

    Inclusão da classe Matriz

---
+++ src/Matriz.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Matriz {
+
+	public static void main(String[] args) {
+		Integer[][] matriz = new Integer[3][3];
+		Integer[] vetor = new Integer[3];
+		
+		List<Integer> lista = new ArrayList<>();
+		
+		lista.add(10);
+		
+		vetor[0] = 5;
+		vetor[1] = 3;
+		vetor[2] = 4;
+		
+		matriz[0][0] = 20;
+		matriz[0][1] = 10;
+		matriz[0][2] = 30;
+		matriz[1][0] = 40;
+
+	}
+
+}
